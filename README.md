b187796794d3e186c3bc4b559f1d2331ee1aa07e
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2021-05-16 15:30:12 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# androidtv-debloat-script
-ADB Shell script to uninstall all bloated apps that comes with some Android TV's
+# Info
+
+* ADB Shell script to uninstall all bloated apps that comes with some Android TV's
+
+# How to run
+
+* Clone this repo
+* First, you need to list out all your Android TV installed apps via the command below
+```bash
+adb shell cmd package list packages "|cut -f 2 -d": >> ~/Desktop/packages.txt
+```
+* Copy all the apps line by line into a `bloat.txt` file in the **SAME** directory with `uninstall-bloat.sh` script
+* Make sure you have `enabled USB debugging` in your `Developer` settings.
+* Run this script using the command below
+```bash
+sh uninstall-bloat.sh
+```
+* Follow the script instructions.
+* Enjoy
+👌
---
+++ uninstall-bloat.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+GREEN='\033[0;32m';
+DEFAULT='\033[0m';
+YELLOW='\033[1;33m';
+CYAN='\033[0;36m';
+
+tv_local_ip='192.168.0.33';
+
+echo "[🔌] Disconnecting from ADB in case is connected somewhere else";
+adb disconnect;
+echo " ";
+
+echo "[🌐] Connecting to ADB on ${GREEN}$tv_local_ip${DEFAULT}:${YELLOW}5555${DEFAULT}";
+adb connect $tv_local_ip:5555 || echo " " && echo "[❗] Please check the TV, enable USB debugging and allow this ADB connection!" && echo "[❗] The script will continue automatically after you allow the connection!";
+adb wait-for-device;
+echo " ";
+
+secs=$((3));
+while [ $secs -gt 0 ]; do
+    printf "\r[⌛] Starting to purge the bloated apps in ${YELLOW}$secs${DEFAULT} seconds...";
+    sleep 1;
+    : $((secs--));
+done;
+
+echo " ";
+echo " ";
+echo "[📝] Purging the bloated apps...";
+echo " ";
+
+cat bloat.txt | while read line; do
+    echo "[👀] Trying to remove ${CYAN}$line${DEFAULT} if is installed";
+    adb uninstall --user 0 $line && adb shell pm clear $line;
+    echo " ";
+    sleep 0.5;
+done;
+
+echo "[👌] Done! Please restart your TV after running this script!";
+echo "[🔌] Disconnecting from ADB again.";
+adb disconnect;
